cf91c5d56829c6b0158598bdc5e6f9832baed8e0
Jose Fajardo <fajardo_jf@hotmail.com>
2014-11-22 11:02:18 +1100

    started adding the xaml parser classes

---
+++ src/XAMLPipeline/XAMLPipeline/Parser/XamlNamespace.ts
@@ -0,0 +1,29 @@
+﻿class XamlNamespace {
+
+    name: string;
+    is_ignored: boolean;
+    prefixes: any = [];
+
+    constructor() {
+        this.name = null;
+        this.prefixes = null;
+        this.is_ignored = false;
+    }
+
+    public FindElement(p: XamlParserInfo, el: string, attr: string, create: boolean): XamlElementInfo { 
+
+        return null;
+    }
+
+    public SetAttribute(p: XamlParserInfo, item : XamlElementInstance, attr: string, value: string): boolean { 
+        return false;
+    }
+
+    public AddPrefix(prefix: string) { }
+    public HasPrefix(prefix: string) : boolean { 
+        return false;
+    }
+    public GetPreixes(): any { 
+        return this.prefixes;
+    }
+}
